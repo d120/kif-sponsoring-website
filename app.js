846928a8df948cd0ab3dd97251123c78e6617f0e
Johannes Lauinger <johannes@lauinger-it.de>
2015-08-12 00:46:46 +0200

    erster Entwurf der Seite, wenig Inhalt

--- app.js
+++ app.js
@@ -4,22 +4,22 @@
 
   $(function(){
     $('.fade-left').css({
-      left: '-200px',
+      left: '-100px',
       opacity: 0
     });
     $('.fade-right').css({
-      left: '+200px',
+      left: '+100px',
       opacity: 0
     })
 
     $('.fade-left, .fade-right').each(function(i, elem){
       new Waypoint.Inview({
         element: elem,
-        enter: function(direction) {
+        entered: function(direction) {
           $(elem).animate({
             left: 0,
             opacity: 1
-          }, 500);
+          }, 400);
         }
       });
     });
